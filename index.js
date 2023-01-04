ec3db6a82ac5ab4ec29d174fe41dc44ffe097dc4
Shaka Suzy <shaheedchambers@gmail.com>
2023-01-03 22:33:14 -0500

    added packages to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Packages for app
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employeeList = [];
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+// Array of questions for user input
---
+++ library/Intern.js
@@ -0,0 +1,14 @@
+const Employee = require("./Employee");
+class Intern extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+  }
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+  getRole() {
+    return "Intern";
+  }
+}
+module.exports = Intern;
